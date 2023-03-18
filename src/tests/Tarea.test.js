1f324c8ddd8d7bdc6bfe86b579af9cbefd44eb0f
adrian-villamil <adrian.villamil@unillanos.edu.co>
2023-03-18 04:54:21 -0500

    Creé multiples tests al componente Tarea.

--- src/tests/Tarea.test.js
+++ src/tests/Tarea.test.js
@@ -1,12 +1,33 @@
-import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 import Tarea from '../components/Tarea';
 
-it('Se renderiza correctamente', () => {
-  const mockCompletarTarea = jest.fn();
-  const mockEliminarTarea = jest.fn();
+describe('Pruebas para el componente Tarea:', () => {
+  test('Se renderiza correctamente el componente Tarea dentro del documento html.', () => {
+    const mockCompletarTarea = jest.fn();
+    const mockEliminarTarea = jest.fn();
 
-  const tree = renderer
-    .create(<Tarea id={0} texto='Levantarme mañana' completada={false} completarTarea={mockCompletarTarea} eliminarTarea={mockEliminarTarea} />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+    const tarea = render(<Tarea id={0} texto='Levantarme mañana' completada={false} completarTarea={mockCompletarTarea} eliminarTarea={mockEliminarTarea} />);
+    expect(tarea.container).toBeInTheDocument();
+  });
+
+  test('Se comprueba que la tarea está completada', () => {
+    const mockCompletarTarea = jest.fn();
+    const mockEliminarTarea = jest.fn();
+
+    render(<Tarea id={0} texto='Dormir bien' completada={true} completarTarea={mockCompletarTarea} eliminarTarea={mockEliminarTarea} />);
+
+    const tarea = document.getElementsByClassName('tarea-contenedor')[0];
+    expect(tarea.classList[1]).toMatch(/completada/);
+  });
+
+  test('Se comprueba que el texto de la tarea sea comer "Hacer ejercicio"', () => {
+    const mockCompletarTarea = jest.fn();
+    const mockEliminarTarea = jest.fn();
+
+    const tarea = render(<Tarea id={0} texto='Hacer ejercicio' completada={true} completarTarea={mockCompletarTarea} eliminarTarea={mockEliminarTarea} />);
+
+    const divTexto = tarea.container.firstChild;
+    expect(divTexto.textContent).toBe('Hacer ejercicio');
+  });
 });
